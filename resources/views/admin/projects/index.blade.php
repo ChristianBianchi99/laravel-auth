3401cb0445bca6531f0738892a4d29596902bf5f
ChristianBianchi99 <bianchi848@gmail.com>
2023-08-21 16:27:44 +0200

    Agg btns crud

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,27 +1,50 @@
 @extends('layouts.admin');
 
 @section('content')
-    <h2>
-        Questi sono i progetti
-    </h2>
-    <table class="table">
-        <tr>
-            <th>
-                Nome
-            </th>
-            <th>
-                Descrizione
-            </th>
-        </tr>
-        @foreach ($projects as $project)
-            <tr>
-                <td>
-                    {{ $project->name }}
-                </td>
-                <td>
-                    {{ $project->description }}
-                </td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-12">
+                <h2 class="my-3">
+                    Projects
+                </h2>
+                <table class="table">
+                    <tr>
+                        <th>
+                            Nome
+                        </th>
+                        <th>
+                            Descrizione
+                        </th>
+                        <th>
+                            Gestisci
+                        </th>
+                    </tr>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>
+                                {{ $project->name }}
+                            </td>
+                            <td>
+                                {{ $project->description }}
+                            </td>
+                            {{-- BTN CRUD --}}
+                            <td>
+                                <div class="btns">
+                                    <button class="btn btn-secondary">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </button>
+                                    <button class="btn btn-info">
+                                        <i class="fa-solid fa-pen"></i>
+                                    </button>
+                                    <button class="btn btn-danger">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\Admin\ProjectController as ProjectController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
